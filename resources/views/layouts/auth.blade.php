d133a81da0ab92aa41ccfd43ea3c42e5013bc812
nang95 <rookie5cookies@gmail.com>
2022-10-11 21:29:49 +0700

    push

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Deliana Inti Sukses') }}</title>
+    <title>Deliana Inti Sukses</title>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -18,13 +18,26 @@
 <body>
     <div id="auth">
         <div class="hello-side">
-            <div class="logo">
-                <img src="{{ asset('images/full_logo_colored.png') }}" width="40%" alt="" class="logo-img">
-            </div>
+            <img src="{{ asset('images/full_logo_colored.png') }}" width="40%" alt="" class="logo-img">
             <div class="overlay"></div>
             <img src="{{ asset('images/background_login.png') }}" class="background-image" alt="" srcset="">
         </div>
-        <div class="box-login"></div>
+        <div class="box-login">
+            <div class="title">Masuk</div>
+            <div class="sub-title">Silahkan masukkan akun terlebih dahulu</div>
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Email...">
+                </div>
+
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Password...">
+                </div>
+
+                <button>Simpan</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
+
 Route::namespace('Front')->group(function(){
     Route::get('/', 'HomeController@index')->name('/');
 });
 
+Route::namespace('Admin')->name('admin.')->prefix('admin')->group(function(){
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
